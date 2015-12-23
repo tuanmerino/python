a25d9c0df575b36cd718896cf592ecd3bd5a242c
Nguyễn Tuấn Anh <iicodonduoimuaii@gmail.com>
2015-12-23 18:03:02 +0700

    truefalse.py
    Chương trình kiểm tra tính đúng sai

---
+++ truefalse.py
@@ -0,0 +1,10 @@
+#program by Tuanmerino
+print "Check true or false"
+a=raw_input(':  ')
+
+def truefalse():
+    if a == True :
+        print "true"
+    else:
+        print "false"
+print truefalse()		
